0cbb0a327963da8245e760644c2eb9ff73f92220
Yasser Friha <friha.yasser@pm.me>
2019-10-30 21:57:21 +0100

    500 error 10

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic'
     'products'
 ]
 
