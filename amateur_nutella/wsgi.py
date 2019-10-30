79d3450c8d679bae617a00454fea095f739048dc
Yasser Friha <friha.yasser@pm.me>
2019-10-30 21:40:21 +0100

    500 error 5

--- amateur_nutella/wsgi.py
+++ amateur_nutella/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'amateur_nutella.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amateur_nutella.settings')
 
 application = get_wsgi_application()
