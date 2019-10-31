47eb42749a8107b43135b2a286a1df2e32b4b2e4
Yasser Friha <friha.yasser@pm.me>
2019-10-31 08:08:51 +0100

    error 500 19

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'oxw^sd1m-dnc4y-#(r#uo#y&68wyzq1g516k4u)5baryeq!6v)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cryptic-headland-44701.herokuapp.com', '127.0.0.1', 'localhost']
 
