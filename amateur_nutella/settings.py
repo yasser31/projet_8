9bd04e23bddbc37646e02c47afc81c3be4157df7
Yasser Friha <friha.yasser@pm.me>
2019-10-31 06:56:57 +0100

    500 error 13

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "products/static")]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 LOGIN_REDIRECT_URL = '/'
