7989f176f2dcce6d6b0a4ce0b73aed8da24e426f
Yasser Friha <friha.yasser@pm.me>
2019-11-04 11:40:44 +0100

    10

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,9 +84,6 @@ DATABASES = {
                     'PORT': '5432',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
