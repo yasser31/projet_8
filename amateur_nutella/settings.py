e651d4aef07507b2f3890d3197c1b8993ee94c06
Yasser Friha <friha.yasser@pm.me>
2019-11-04 12:03:31 +0100

    15

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,5 +146,3 @@ LOGGING = {
         },
     },
 }
-
-django_heroku.settings(locals())
