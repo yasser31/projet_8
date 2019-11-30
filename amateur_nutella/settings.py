32eeac9d1ab481ac7fbd3ceec5d0cd8287bc45c2
Yasser Friha <friha.yasser@pm.me>
2019-11-30 09:28:30 +0100

    debug False

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cryptic-headland-44701.herokuapp.com',
                  '127.0.0.1', 'localhost']
