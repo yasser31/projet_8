915ba84de130824cb9b0e3fdc634eba895166740
Yasser Friha <friha.yasser@pm.me>
2020-01-08 16:32:34 +0100

    the last commit

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cryptic-headland-44701.herokuapp.com',
                  '127.0.0.1', 'localhost', "167.71.74.215"]
