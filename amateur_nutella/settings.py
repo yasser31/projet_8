940398ccefae1e9a5b37f883e52e193eb51562aa
Yasser Friha <friha.yasser@pm.me>
2020-01-08 16:38:18 +0100

    the last commit 1

--- amateur_nutella/settings.py
+++ amateur_nutella/settings.py
@@ -10,17 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://a00b4f587b17408db157e097d409d893@sentry.io/1870977",
-    integrations=[DjangoIntegration()],
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
